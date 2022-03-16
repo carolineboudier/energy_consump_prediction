b9165cb2e90d243f8e21c6d2c6c345c528491f96
admin <caroline.boudier@dataiku.com>
2022-03-16 19:03:59 +0100

    Saved web app Predict building energy use intensity (ENERGYCONSUMPTIONPREDICTION.2JY0YEG)

--- web_apps/2JY0YEG/backend.py
+++ web_apps/2JY0YEG/backend.py
@@ -21,7 +21,7 @@ def scoring_api_call(params):
     if params.get('year_built') == "":
         year_built = "1950"
     else :
-        year_built=2001
+        year_built=params.get('year_built')
 
     
     record_to_predict = {
@@ -40,6 +40,8 @@ def scoring_api_call(params):
     prediction = client.predict_record("predict_energy_use_intensity", record_to_predict)
     print("prediction sent back to JS",prediction)
     precise_pred=prediction["result"]["prediction"]
+    if precise_pred>=500:
+        precise_pred=500
     print("precise pred sent back to JS",precise_pred)
 
     return json.dumps({"results": precise_pred})
