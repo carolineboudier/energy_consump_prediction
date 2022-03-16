cb0eafe47fc6cd0828761ef9e548d99816cf7206
admin <caroline.boudier@dataiku.com>
2022-03-16 18:55:40 +0100

    Saved web app Predict building energy use intensity (ENERGYCONSUMPTIONPREDICTION.2JY0YEG)

--- web_apps/2JY0YEG/app.js
+++ web_apps/2JY0YEG/app.js
@@ -193,7 +193,7 @@ function onDocumentReady(val) {
     
     powerGauge.render(val*100);
         
-    let legend = "Score = " + val*100;
+    let legend = "Score = " + val;
     legend = legend.substring(0, 12) + "%";
     legendGauge.innerHTML = "<i>" + legend + "</i>";   
     
