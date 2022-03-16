3b1aab941d4c9853e2c1bb7edc29dbf6661efb3e
admin <caroline.boudier@dataiku.com>
2022-03-16 17:29:44 +0100

    Saved web app Predict building energy use intensity (ENERGYCONSUMPTIONPREDICTION.2JY0YEG)

--- web_apps/2JY0YEG/app.js
+++ web_apps/2JY0YEG/app.js
@@ -244,8 +244,3 @@ validationButton.addEventListener('click', function (event) {
     
     run_scoring(variables);
     event.preventDefault();
-});$.getJSON(getWebAppBackendUrl('/first_api_call'), function(data) {
-    console.log('Received data from backend', data)
-    const output = $('<pre />').text('Backend reply: ' + JSON.stringify(data));
-    $('body').append(output)
-});
