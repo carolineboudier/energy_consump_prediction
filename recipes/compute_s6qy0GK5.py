3d152b977a9bebed3a82678721b1bdacc1d261fa
admin <caroline.boudier@dataiku.com>
2022-03-22 18:30:34 +0100

    Recipe ENERGYCONSUMPTIONPREDICTION.compute_s6qy0GK5 saved through notebook notebook editor for compute_s6qy0GK5

--- recipes/compute_s6qy0GK5.py
+++ recipes/compute_s6qy0GK5.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -7,9 +8,19 @@ from dataiku import pandasutils as pdu
 data_by_state_filtered_ordered = dataiku.Dataset("data_by_state_filtered_ordered")
 data_by_state_filtered_ordered_df = data_by_state_filtered_ordered.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Recipe outputs
+import os
+folder_handle = dataiku.Folder("s6qy0GK5")
+folder_path = folder_handle.get_path()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+path_output_folder="datasets"
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+final_path=os.path.join(folder_path,path_output_folder,"my_file.csv")
+final_path
 
-# Write recipe outputs
-output_datasets = dataiku.Folder("s6qy0GK5")
-output_datasets_info = output_datasets.get_info()
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+with folder_handle.get_writer(final_path) as writer:
+    writer.write(data_by_state_filtered_ordered_df.to_csv().encode("utf-8"))
