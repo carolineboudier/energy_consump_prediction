77252aaa18a4da43a7a052b1ac4cab4cac9fb057
admin <caroline.boudier@dataiku.com>
2022-03-22 18:32:58 +0100

    Recipe ENERGYCONSUMPTIONPREDICTION.compute_s6qy0GK5 saved through notebook notebook editor for compute_s6qy0GK5

--- recipes/compute_s6qy0GK5.py
+++ recipes/compute_s6qy0GK5.py
@@ -3,23 +3,23 @@
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
-
-# Read recipe inputs
-data_by_state_filtered_ordered = dataiku.Dataset("data_by_state_filtered_ordered")
-data_by_state_filtered_ordered_df = data_by_state_filtered_ordered.get_dataframe()
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Recipe outputs
 import os
-folder_handle = dataiku.Folder("s6qy0GK5")
-folder_path = folder_handle.get_path()
+
+# Read recipe inputs and get a dataframe
+dataset = dataiku.Dataset("data_by_state_filtered_ordered")
+df = dataset.get_dataframe()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# folder handle
+folder_handle = dataiku.Folder("s6qy0GK5")
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# location wished in the output folder
 path_output_folder="datasets"
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-final_path=os.path.join(folder_path,path_output_folder,"my_file.csv")
+final_path=os.path.join(path_output_folder,"my_file.csv")
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 with folder_handle.get_writer(final_path) as writer:
-    writer.write(data_by_state_filtered_ordered_df.to_csv().encode("utf-8"))
+    writer.write(df.to_csv().encode("utf-8"))
