2a29351ce1cb79e0c7a227cf6c13c8e1daacf271
admin <caroline.boudier@dataiku.com>
2022-03-31 10:40:29 +0200

    Saved recipe: ENERGYCONSUMPTIONPREDICTION.compute_full_data_with_code

--- recipes/compute_full_data_with_code.py
+++ recipes/compute_full_data_with_code.py
@@ -8,12 +8,6 @@ full_data_prepared = dataiku.Dataset("full_data_prepared")
 full_data_prepared_df = full_data_prepared.get_dataframe(infer_with_pandas=False)
 infered_df = full_data_prepared.get_dataframe()
 
-
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
-
-
 # Write recipe outputs
 full_data_with_code = dataiku.Dataset("full_data_with_code")
 full_data_with_code.write_with_schema(full_data_prepared_df)
